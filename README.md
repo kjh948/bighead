bfd33c468f7b0c4d11365b1e5a6fff7899236b0e
kjh948 <kjh948@gmail.com>
2025-02-22 23:35:16 +0900

    nav done

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # bighead
+
+1. bringup
+ 1.1 source all env variables
+ 1.2 ros2 launch omo_r1mini_bringup omo_r1mini_mcu.launch.py or ros2 launch omo_r1mini_bringup omo_r1mini_bringup.launch.py
+ 1.3 ros2 run omo_r1mini_teleop teleop_keyboard
+2. run slam
+ 2.1 ros2 launch omo_r1mini_cartographer cartographer.launch.py
+3. remote pc 
+ 2.1 ros2 launch omo_r1mini_cartographer cartographer_rviz.launch.py
+ 2.2 ros2 run nav2_map_server map_saver_cli -f map
+
+4. nav2
+ 4.1 ros2 launch omo_r1mini_navigation2 navigation2.launch.py map:=$HOME/map.yaml
+ 4.1 ros2 launch omo_r1mini_navigation2 navigation2_rviz.launch.py @ remote pc
+
+#env
+export DISPLAY=:0
+
+
+#ref
+https://t-shaped-person.tistory.com/198
--- src/omo_r1mini-foxy/omo_r1mini_bringup/create_udev_rules.sh
+++ src/omo_r1mini-foxy/omo_r1mini_bringup/create_udev_rules.sh
@@ -8,7 +8,7 @@ echo "Motor Driver (USB Serial from RS232) : /dev/ttyTHS1 to /dev/ttyMCU :"
 if [ -f "/etc/udev/rules.d/98-omo-r1-mini-mcu" ]; then
     echo "98-omo-r1-mini-mcu file already exist."
 else 
-    echo 'KERNEL=="ttyTHS1", MODE:="0666", GROUP:="dialout", SYMLINK+="ttyMCU"' > /etc/udev/rules.d/98-omo-r1-mini-mcu.rules   
+    echo 'KERNEL=="ttyUSB1",  ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", MODE:="0666", GROUP:="dialout",   SYMLINK+="ttyMCU"' > /etc/udev/rules.d/98-omo-r1-mini-mcu.rules   
     echo '98-omo-r1-mini-mcu created'
 fi
 
@@ -17,16 +17,23 @@ echo "YD LiDAR (USB Serial) : /dev/ttyUSBx to /dev/ttyLiDAR :"
 if [ -f "/etc/udev/rules.d/97-omo-r1-mini-lidar.rules" ]; then
     echo "97-omo-r1-mini-lidar.rules file already exist."
 else 
-    echo 'KERNEL=="ttyUSB*", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", MODE:="0666", GROUP:="dialout",  SYMLINK+="ttyLiDAR"' >/etc/udev/rules.d/97-omo-r1-mini-lidar.rules
+    echo 'KERNEL=="ttyUSB0",  ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", MODE:="0666", GROUP:="dialout",  SYMLINK+="ttyLiDAR"' >/etc/udev/rules.d/97-omo-r1-mini-lidar.rules
     
     echo '97-omo-r1-mini-lidar.rules created'
 fi
 
-systemctl stop nvgetty
-systemctl disable nvgetty
+#systemctl stop nvgetty
+#systemctl disable nvgetty
 
 echo ""
 echo "Reload rules"
 echo ""
 sudo udevadm control --reload-rules
 sudo udevadm trigger
+
+#mcu - usb1
+#BUS="usb", ID=="1-2.4:1.0", SYMLINK+="ttyMCU"
+#lidar - usb0
+#BUS="usb", ID=="1-2.3:1.0", SYMLINK+="ttyLiDAR"
+
+#echo 'KERNEL=="ttyUSB*", MODE:="0666", GROUP:="dialout", SYMLINK+="ttyMCU"'
